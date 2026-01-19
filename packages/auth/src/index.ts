94492d13b6caf42eaf44b0346cd53fa1d77866b0
Jose Manuel Veliz <jose.manuel.veliz.p@gmail.com>
2026-01-19 00:03:15 -0400

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,42 @@
+import { db } from "@acontia/db";
+import * as schema from "@acontia/db/schema/auth";
+import { env } from "@acontia/env/server";
+import { polar, checkout, portal } from "@polar-sh/better-auth";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+import { polarClient } from "./lib/payments";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      enableCustomerPortal: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "your-product-id",
+              slug: "pro",
+            },
+          ],
+          successUrl: env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+    nextCookies(),
+  ],
+});
